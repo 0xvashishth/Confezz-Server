232f68ddb28cf924fd447a41b88b5209b05fa7e1
Vashishth Patel <76911582+vasu-1@users.noreply.github.com>
2022-11-19 08:55:58 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+// const connectDB = require('./config/db');
+var cors = require('cors');
+const bodyParser = require('body-parser')
+
+const app = express();
+//body-parse
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+// cors 
+app.use(cors({ origin: true, credentials: true }));
+
+// Connect Database
+// connectDB();
+
+
+// Init Middleware
+app.use(express.json({ extended: false }));
+app.use(express.json());
+
+const middleware = (req, res, next) => {
+  console.log("Hello my middleware");
+  next();
+}
+
+
+app.get('/', (req, res) => {
+  console.log("Hello Askoverflosw!!");
+  res.send('Hello Askoverflosw!!')
+});
+
+app.get('/contact', middleware, (req, res) => {
+  console.log("Hello my contact");
+  res.send('Hello Contact')
+});
+
+const port = process.env.PORT || 8082;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
